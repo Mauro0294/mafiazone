d8d2b2aa40dc89666811ee26b5b6bff2cd6695d5
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-08-01 00:15:06 +0200

    feat: werkende koffers

---
+++ koffers.php
@@ -0,0 +1,65 @@
+<?php
+include "notLoggedIn.php";
+
+$username = $_SESSION['username'];
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="koffers.css">
+        <script src="https://kit.fontawesome.com/44d0f25a2a.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h2>Koffers</h2>
+        <p>Koffers vind je tijdens het spelen van MafiaZone. Ze bevatten cash, kogels en credits! Je ontvangt een bericht wanneer je een koffer gevonden hebt.<br /><br /> Klik op een koffer, om hem te openen, je ziet meteen, wat erin zit.<br /> <br />
+        Je hebt momenteel <?php $stmt = $pdo->prepare("SELECT koffers FROM users WHERE gebruikersnaam = :username");
+    $stmt->execute(['username' => $username]);
+    $kofferfetch = $stmt->fetch();
+    $koffer = $kofferfetch['koffers'];
+    echo $koffer ?> koffers
+        <div>
+        <form method="POST">
+            <?php
+            for ($i = 0; $i < $koffer; $i++) {
+                    echo "<button type='submit' name='submit' style='margin-right: 10px; margin-bottom: 10px;'>
+                    <i id='koffer_icon' class='fa fa-briefcase' aria-hidden='true'></i>
+                    </button>";
+                }
+             ?>
+        </form>
+        </div>
+    </body>
+</html>
+
+<?php
+if (isset($_POST['submit'])) {
+    if ($koffer < 0) return;
+    $stmt = $pdo->prepare("UPDATE users SET koffers = koffers - 1 WHERE gebruikersnaam = :username");
+    $stmt->execute(['username' => $username]);
+
+    $keuze = rand(0, 2);
+    switch ($keuze) {
+        case 0:
+            $randommoney = rand(50000, 200000);
+            $stmt = $pdo->prepare("UPDATE users SET cashgeld = cashgeld + :randommoney WHERE gebruikersnaam = :username");
+            $stmt->execute(['randommoney' => $randommoney, 'username' => $username]);
+            header("Refresh: 0");
+            return;
+        case 1:
+            $randomcredits = rand(1, 10);
+            $stmt = $pdo->prepare("UPDATE users SET credits = credits + :randomcredits WHERE gebruikersnaam = :username");
+            $stmt->execute(['randomcredits' => $randomcredits, 'username' => $username]);
+            header("Refresh: 0");
+            return;
+        case 2:
+            $randombullets = rand(10, 100);
+            $randommoney = rand(50000, 200000);
+            $stmt = $pdo->prepare("UPDATE users SET kogels = kogels + :randombullets WHERE gebruikersnaam = :username");
+            $stmt->execute(['randombullets' => $randombullets, 'username' => $username]);
+            $stmt = $pdo->prepare("UPDATE users SET cashgeld = cashgeld + :randommoney WHERE gebruikersnaam = :username");
+            $stmt->execute(['randommoney' => $randommoney, 'username' => $username]);
+            header("Refresh: 0");
+            return;
+    }
+}
+?>
